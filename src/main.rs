27857481b89360128d4d6b6cefbd00307e38aabd
Jascha030 <contact@jaschavanaalst.nl>
2022-07-10 04:01:06 +0200

    🚧 Test figlet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use figlet_rs::FIGfont;
+
 fn main() {
-    println!("Hello, world!");
+    let font = FIGfont::from_file("resources/roman.flf").unwrap();
+    let figure = font.convert("title");
+
+    print!("{}", figure.unwrap().to_string())
 }
