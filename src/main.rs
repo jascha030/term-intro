6cd845b67f5c95f0a7f885628a0c329564a714f4
Jascha030 <contact@jaschavanaalst.nl>
2022-07-10 04:01:15 +0200

    🚚 Add font

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use figlet_rs::FIGfont;
 
 fn main() {
     let font = FIGfont::from_file("resources/roman.flf").unwrap();
-    let figure = font.convert("title");
+    let figure = font.convert("Hackerman Mode 030");
 
     print!("{}", figure.unwrap().to_string())
 }
