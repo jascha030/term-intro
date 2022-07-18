d8d5e42a130cf2edbd09bfff3c83aa055a5381ab
Jascha030 <contact@jaschavanaalst.nl>
2022-07-18 21:18:16 +0200

    ✨ Add struct.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
 use figlet_rs::FIGfont;
+use std::io::Result;
+use std::vec::Vec;
 
-fn main() {
+#[derive(Debug)]
+struct FIGDimmensions {
+    width: u32,
+    rows: u32,
+    line_height: u32,
+    characters: u32,
+    breakpoints: Vec<u32>,
+}
+
+
+fn main() -> Result<()> {
     let font = FIGfont::from_file("resources/roman.flf").unwrap();
     let figure = font.convert("Hackerman Mode 030");
 
